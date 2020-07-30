a49e0a5755cdaf5a7c2c24b598094ff8a1d4bccc
Rahul Kadam <rahul-dnyaneshwar_kadam@astro.com.my>
2020-07-30 10:07:07 +0530

    #7 excel file support basic structure

---
+++ src/main/java/com/garv/satta/fantasy/service/ExcelFileService.java
@@ -0,0 +1,44 @@
+package com.garv.satta.fantasy.service;
+
+import com.garv.satta.fantasy.exceptions.GenericException;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Iterator;
+
+/**
+ * Reference
+ * https://bezkoder.com/spring-boot-upload-excel-file-database/
+ */
+@Service
+public class ExcelFileService {
+
+    public static String TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+    public static boolean hasExcelFormat(MultipartFile file) {
+
+        if (!TYPE.equals(file.getContentType())) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public Iterator<Row> readData(MultipartFile file, String sheetname) {
+        try {
+            if (!hasExcelFormat(file)) {
+                throw new GenericException("Invalid file format");
+            }
+            Workbook workbook = new XSSFWorkbook(file.getInputStream());
+            Sheet sheet = workbook.getSheet(sheetname);
+            Iterator<Row> rows = sheet.iterator();
+            return rows;
+        } catch (Exception e) {
+            throw new GenericException("exception while reading file " + e.getMessage());
+        }
+    }
+}
