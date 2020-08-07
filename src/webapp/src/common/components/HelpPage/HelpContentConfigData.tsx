1e80c89475a2aaa72d26c283c46fc444a17d052b
Rahul Kadam <rahul-dnyaneshwar_kadam@astro.com.my>
2020-08-07 16:19:03 +0530

    #52 adding changes for help apge improvement

--- src/webapp/src/common/components/HelpPage/HelpContentConfigData.tsx
+++ src/webapp/src/common/components/HelpPage/HelpContentConfigData.tsx
@@ -1,95 +1,44 @@
 export const HelpContentConfigData = {
   HelpContent: [
     {
-      id: 'quickpay',
-      topicName: 'Quick Pay',
+      id: 'fantasy',
+      topicName: 'Sport Fantasy',
       subtopics: [
         {
-          question: 'What is Astro Quick Pay?',
+          question: 'What is Sport Fantasy?',
           answer:
-            "Astro Quick Pay is a convenient method to check your Bill Summary, Account Status and pay Astro bills at your convenience with immediate payment posting, right from home. Visit <a href='/'>Astro Quick Pay</a> to pay your bill now.",
+            'Sport fantasy is for Sport fan who want to play league with single team only, with Transfer restriction',
         },
         {
-          question: 'What information is required to pay my Astro bill?',
+          question: 'What information is required to Play Sport Fantasy?',
           answer:
-            'You would need your 10-digit account number and your email address.',
+            'You would need to login to Fantasy site, create team, join any league or create League',
         },
         {
-          question: 'What is my account number?',
+          question: 'What is my User Team?',
           answer:
-            'Your Astro account number is a 10-digit combination e.g. 08XXXXXXXX.  You may log on to Astro Self Service or refer to the top right-hand corner of your bill.',
-        },
-        {
-          question: 'Is my personal information protected by the PDPA?',
-          answer:
-            "Yes, Astro is bounded by the PDPA act. All information received will be used in accordance with our <a href='https://www.astro.com.my/privacy-notice'>Privacy Notice</a>.",
-        },
-        {
-          question:
-            'Are there any extra charges incurred if I make a payment through Astro Quick Pay?',
-          answer:
-            'Of course not, it is a value-added service introduced for your convenience.',
+            'Your User team will the the team which you will create only once, and then you will use transfer to play fantasy.',
         },
       ],
     },
     {
-      id: 'billing',
-      topicName: 'Billing',
+      id: 'transfer',
+      topicName: 'Transfer',
       subtopics: [
         {
-          question:
-            'What happens if I pay more than what is stated in my bill?',
+          question: 'What happens if I finished all transfer?',
           answer:
-            'The balance will be credited into your account, which will be reflected when you receive your next bill. The balance credit will be used to offset your bill balance.',
+            'You have to use your transfer properly, we prefer that you should not user more that 3-4 transfer per match, so you can continue playing fantasy with till end',
         },
         {
-          question:
-            'What do I do if I still receive SMS payment reminders even though I have already made payment?',
+          question: 'Can we transfer player inbetween match',
           answer:
-            'Please disregard the reminder if payment had been made and do check if you have any outstanding balance on <a href="/">Astro Quick Pay</a>.',
+            'Nope, you can not transfer playing during live match, as. If you made it, it will be used in next match',
         },
         {
-          question: 'My account is disconnected how do I reconnect my account?',
+          question: 'When can I see My Fantasy score?',
           answer:
-            'Your disconnected services would resume within 1 hour after you have paid your "Total Overdue Charges".  Please check your Account status, Bill Summary and pay on <a href=\'/\'>Astro Quick Pay</a>.',
-        },
-        {
-          question:
-            'I have sufficient funds in my account. Why is my payment unsuccessful?',
-          answer:
-            "Please check SMS received on the unsuccessful payment status and should you require further clarification contact your issuing bank for clarification or contact us via <a href='https://api.whatsapp.com/send?phone=60395433838&text=Hi'>WhatsApp</a>.",
-        },
-      ],
-    },
-    {
-      id: 'apg',
-      topicName: 'APG',
-      subtopics: [
-        {
-          question: 'What payment methods are available in Astro Quick Pay?',
-          answer:
-            '<ol>' +
-            '<li>' +
-            'Credit or Debit Cards' +
-            '<ul>' +
-            '<li>Visa</li>' +
-            '<li>Mastercard</li>' +
-            '</ul>' +
-            '</li>' +
-            '<li>Maybank2U - Pay using your Maybank2U account.</li>' +
-            '<li>FPX - Pay using any local bank account via FPX.</li>' +
-            '</ol>',
-        },
-        {
-          question:
-            'What is "Add Billing Reminder to Google Calendar" and how does it help?',
-          answer:
-            'You can add a reminder to pay your bill, on your Google Calendar with one click. Enjoy uninterrupted services, by remembering to pay your bill on time.',
-        },
-        {
-          question: 'Further Help!',
-          answer:
-            "For any issues, contact us via <a href='https://api.whatsapp.com/send?phone=60395433838&text=Hi'>WhatsApp</a>.",
+            'Fantasy score, will get updated after match only once, we are not providing live score.',
         },
       ],
     },
--- src/webapp/src/common/components/HelpPage/HelpPage.tsx
+++ src/webapp/src/common/components/HelpPage/HelpPage.tsx
@@ -82,34 +82,10 @@ const HelpPage = () => {
   return (
     <div>
       <Helmet>
-        <title>Help - Astro Payment Gateway</title>
-        <meta
-          name="description"
-          content="Astro Payment Gateway Help & Support - Get help on payment, billing issues, payment methods, and how to reconnect your account; learn more about Astro Payment Gateway and other FAQs"
-        />
-        <meta
-          property="og:title"
-          content="Astro Payment Help | Astro Payment Gateway Help | Astro Billing Help "
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://pay.astro.com.my/apg/help" />
-        <link rel="canonical" href="https://pay.astro.com.my/apg/help" />
-        <meta property="og:site_name" content="Astro Payment Gateway" />
-        <meta
-          property="og:description"
-          content="Help and FAQ for Astro Payment Gateway"
-        />
-        <meta
-          property="og:image"
-          content="<%=host%>/Resources/images/logos/astro_share.png"
-        />
-        <meta
-          name="keywords"
-          content="astro bill, astro quick pay, astro pay bill, quick pay astro bill payment, online bill, quick bill payment, astro bill help, faq, info, how to pay astro bill, astro payment gateway help, astro payment gateway faq, astro bill pay faq, astro bill payment faq, astro bill payment help, what is quick pay, what is quickpay, what is apg, what is my astro account number, what is my account number, astro account disconnected, astro account not available, astro payment unsuccessful, astro reconnect account, astro payment extra charges, astro payment methods"
-        />
+        <title>Help - Sport Fantasy</title>
       </Helmet>
       <Container className="mt-3">
-        <h4 className="text-center mb-4">Astro Payment Gateway Help</h4>
+        <h4 className="text-center mb-4">Sport Fantasy Help</h4>
         <Accordion defaultActiveKey={firstTopicId + 'Target'}>
           {renderHelpContent()}
         </Accordion>
