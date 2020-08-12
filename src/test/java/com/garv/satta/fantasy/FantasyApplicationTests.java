ceb2986c04dfc59bfdea6a65b90ebad86f27e1d3
Rahul Kadam <rahul-dnyaneshwar_kadam@Astro.com.my>
2020-08-12 16:35:50 +0530

    #68 gcp support

--- src/test/java/com/garv/satta/fantasy/FantasyApplicationTests.java
+++ src/test/java/com/garv/satta/fantasy/FantasyApplicationTests.java
@@ -3,10 +3,9 @@ package com.garv.satta.fantasy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class FantasyApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
--- src/webapp/src/features/Authentication/redux/auth-util.ts
+++ src/webapp/src/features/Authentication/redux/auth-util.ts
@@ -1,8 +1,29 @@
 import {UserLogOutActions} from './authenticationActions';
+import {domainName} from '../../../API';
 
 export function checkUserAccess(message: string) {
   const UserLogOut = UserLogOutActions();
   if (message == 'Unauthorized') {
     UserLogOut();
   }
+}
+
+export function getRedirectUrlforLocal() {
+  const domain = domainName();
+  let redirectUrl = domain;
+  const port = window && window.location && window.location.port;
+  const hostname = window && window.location && window.location.hostname;
+  if (domain.includes('localhost') && port != '8080') {
+    redirectUrl = 'http://' + hostname + port;
+  }
+  redirectUrl = redirectUrl + '/oauth2/redirect';
+  return redirectUrl;
+}
+
+export function getLoginRedirectionUrl() {
+  const domain = domainName();
+  const redirectUrl = getRedirectUrlforLocal();
+  const GOOGLE_AUTH_URL =
+    domain + '/oauth2/authorize/google?redirect_uri=' + redirectUrl;
+  return GOOGLE_AUTH_URL;
 }
